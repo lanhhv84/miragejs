60b16c31ac2bcfc8b6d3fb7940fe3568ff344ce3
lanhhv84 <lanhhv84@gmail.com>
2022-11-26 00:08:52 +0800

    Convert container

--- lib/container.ts
+++ lib/container.ts
@@ -1,4 +1,4 @@
-import { singularize, pluralize } from "inflected";
+import { pluralize, singularize } from "inflected";
 import Db from "./db";
 import Association from "./orm/associations/association";
 import RouteHandler from "./route-handler";
@@ -6,6 +6,10 @@ import BaseRouteHandler from "./route-handlers/base";
 import Serializer from "./serializer";
 import SerializerRegistry from "./serializer-registry";
 import Schema from "./orm/schema";
+
+const defaultInflector = { singularize, pluralize };
+
+type StringToString = (value: string) => string;
 
 const classes = {
   Db,
@@ -17,47 +21,55 @@ const classes = {
   Schema,
 };
 
-let defaultInflector = { singularize, pluralize };
-
 /**
-  Lightweight DI container for customizable objects that are needed by
-  deeply nested classes.
+ Lightweight DI container for customizable objects that are needed by
+ deeply nested classes.
 
-  @class Container
-  @hide
+ @class Container
+ @hide
  */
 class Container {
+  readonly inflector: Record<string, StringToString>;
+
   constructor() {
     this.inflector = defaultInflector;
   }
 
-  register(key, value) {
-    this[key] = value;
+  register(key: string, value: StringToString) {
+    this.inflector[key] = value;
   }
 
-  create(className, ...args) {
-    let Class = classes[className];
+  create<ClassName extends keyof typeof classes>(
+    className: ClassName,
+    ...args: ConstructorParameters<typeof classes[typeof className]>
+  ) {
+    const Class = classes[className];
+    // FIXME: Remove this @ts-ignore after finished migrating all the Class in classes
+    // @ts-ignore
     Class.prototype._container = this;
 
+    // FIXME
+    // @ts-ignore
     return new Class(...args);
   }
 }
 
-/**
-  These are side effects. We give each class a default container so it can be
-  easily unit tested.
+const defaultContainer = new Container();
 
-  We should remove these once we have test coverage and can refactor to a proper
-  DI system.
-*/
-let defaultContainer = new Container();
-
+// FIXME: Remove these @ts-ignore after finished migrating all the Class in classes
+// @ts-ignore
 Db.prototype._container = defaultContainer;
+// @ts-ignore
 Association.prototype._container = defaultContainer;
+// @ts-ignore
 BaseRouteHandler.prototype._container = defaultContainer;
+// @ts-ignore
 RouteHandler.prototype._container = defaultContainer;
+// @ts-ignore
 Serializer.prototype._container = defaultContainer;
+// @ts-ignore
 SerializerRegistry.prototype._container = defaultContainer;
+// @ts-ignore
 Schema.prototype._container = defaultContainer;
 
 export default Container;
